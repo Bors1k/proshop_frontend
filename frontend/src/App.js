0fc9b7446e67db5ef2876fc9ef6b42dc65077d45
Bors1k <8916misha@gmail.com>
2022-01-14 17:07:41 +0300

    init react project

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <main className='py-5'>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+        </main>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
