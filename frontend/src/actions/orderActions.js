f4d159b333d4bcce8f8ff9b5f40ce64066663250
Bors1k <8916misha@gmail.com>
2022-02-02 23:02:15 +0300

    add OrderScreen

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,19 +1,67 @@
-import axios from 'axios';
+import axios from 'axios'
 
 import {
-    ORDER_CREATE_FAIL,
-    ORDER_CREATE_SUCCESS,
-    ORDER_CREATE_REQUEST,
-    
-} from '../constants/orderConstans';
+  ORDER_CREATE_FAIL,
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_REQUEST,
+  ORDER_DETAILS_FAIL,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_REQUEST,
+} from '../constants/orderConstans'
 
-import {CART_CLEAR_ITEMS} from '../constants/cartConstants';
-
+import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
 
 export const createOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_CREATE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${userInfo.access}`,
+      },
+    }
+
+    const { data } = await axios.post(`/api/orders/add/`, order, config)
+
+    dispatch({
+      type: ORDER_CREATE_SUCCESS,
+      payload: data,
+    })
+
+    dispatch({
+      type: CART_CLEAR_ITEMS,
+      payload: data,
+    })
+
+    localStorage.removeItem('cartItems')
+
+    dispatch({
+      type: CART_CLEAR_ITEMS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    })
+  }
+}
+
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
     try {
       dispatch({
-        type: ORDER_CREATE_REQUEST,
+        type: ORDER_DETAILS_REQUEST,
       })
   
       const {
@@ -24,32 +72,19 @@ export const createOrder = (order) => async (dispatch, getState) => {
         headers: {
           'Content-type': 'application/json',
           Authorization: `Bearer ${userInfo.access}`,
-  
         },
       }
   
-      const { data } = await axios.post(`/api/orders/add/`, order, config)
+const { data } = await axios.get(`/api/orders/${id}`, config)
   
       dispatch({
-        type: ORDER_CREATE_SUCCESS,
+        type: ORDER_DETAILS_SUCCESS,
         payload: data,
       })
-
-      dispatch({
-        type: CART_CLEAR_ITEMS,
-        payload: data,
-      })
-
-      localStorage.removeItem('cartItems')
   
-      dispatch({
-        type: CART_CLEAR_ITEMS,
-        payload: data,
-      })
-
     } catch (error) {
       dispatch({
-        type: ORDER_CREATE_FAIL,
+        type: ORDER_DETAILS_FAIL,
         payload:
           error.response && error.response.data.detail
             ? error.response.data.detail
